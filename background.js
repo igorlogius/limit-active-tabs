57d9ca25cba1b68a95b510ae25edecdce1512a40
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-11-29 17:28:48 +0100

    fix(#1): show toolbar button after inital installation

--- background.js
+++ background.js
@@ -4,48 +4,59 @@ const excluded = new Set();
 let tid = null;
 
 async function getFromStorage(type, id, fallback) {
-    let tmp = await browser.storage.local.get(id);
-    return (typeof tmp[id] === type) ? tmp[id] : fallback;
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
 }
 
 async function doUnload() {
-	const tabs = await browser.tabs.query({url: "*://*/*", currentWindow: true, pinned: false, discarded: false });
-    const maxactivtabs = await getFromStorage('number', 'maxactivtabs', 3);
+  const tabs = await browser.tabs.query({
+    url: "*://*/*",
+    currentWindow: true,
+    pinned: false,
+    discarded: false,
+  });
+  const maxactivtabs = await getFromStorage("number", "maxactivtabs", 3);
 
-	// remove excluded and order tabs (by last accessed time
-	let tabIds = tabs.filter(t => (!excluded.has(t.id)) )
-                     .sort( (a, b) => { return (b.lastAccessed - a.lastAccessed) } )
-                     .map( t => t.id );
+  // remove excluded and order tabs (by last accessed time
+  let tabIds = tabs
+    .filter((t) => !excluded.has(t.id))
+    .sort((a, b) => {
+      return b.lastAccessed - a.lastAccessed;
+    })
+    .map((t) => t.id);
 
-    // remove user defined number of last accessed tabs
-    if(tabIds.length > maxactivtabs){
-        tabIds = tabIds.slice(maxactivtabs);
-        browser.tabs.discard(tabIds);
-    }
+  // remove user defined number of last accessed tabs
+  if (tabIds.length > maxactivtabs) {
+    tabIds = tabIds.slice(maxactivtabs);
+    browser.tabs.discard(tabIds);
+  }
 }
 
-async function onClicked(/*tab ,clickData*/){
-    const tabs = await browser.tabs.query({currentWindow: true, highlighted: true});
-    for(const tab of tabs) {
-        if( excluded.has(tab.id) ){
-            excluded.delete(tab.id);
-            browser.browserAction.setBadgeText({tabId: tab.id, text: "" });
-        }else{
-            excluded.add(tab.id);
-            browser.browserAction.setBadgeText({tabId: tab.id, text: "off" });
-        }
+async function onClicked(/*tab ,clickData*/) {
+  const tabs = await browser.tabs.query({
+    currentWindow: true,
+    highlighted: true,
+  });
+  for (const tab of tabs) {
+    if (excluded.has(tab.id)) {
+      excluded.delete(tab.id);
+      browser.browserAction.setBadgeText({ tabId: tab.id, text: "" });
+    } else {
+      excluded.add(tab.id);
+      browser.browserAction.setBadgeText({ tabId: tab.id, text: "off" });
     }
+  }
 }
 
 function onRemoved(tabId /*,removeInfo*/) {
-    if (excluded.has(tabId)) {
-        excluded.delete(tabId);
-    }
+  if (excluded.has(tabId)) {
+    excluded.delete(tabId);
+  }
 }
 
-function delay_unload(){
-    clearTimeout(tid);
-    tid = setTimeout(doUnload, 2000);
+function delay_unload() {
+  clearTimeout(tid);
+  tid = setTimeout(doUnload, 2000);
 }
 
 // default state is disabled
@@ -55,20 +66,22 @@ browser.browserAction.disable();
 browser.browserAction.onClicked.addListener(onClicked);
 browser.tabs.onRemoved.addListener(onRemoved);
 browser.tabs.onActivated.addListener(delay_unload);
-browser.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
-        if(/^https?:/.test(tab.url)){
-            if(changeInfo.status === 'complete'){
-                if (excluded.has(tabId)) {
-                    browser.browserAction.setBadgeText({"tabId": tabId, text: "off" });
-                }else{
-                    browser.browserAction.setBadgeText({"tabId": tabId, text: "" });
-                }
-                browser.browserAction.enable(tabId);
-            }else{
-                delay_unload();
-            }
-        }else{
-            browser.browserAction.disable(tabId);
+browser.tabs.onUpdated.addListener(
+  (tabId, changeInfo, tab) => {
+    if (/^https?:/.test(tab.url)) {
+      if (changeInfo.status === "complete") {
+        if (excluded.has(tabId)) {
+          browser.browserAction.setBadgeText({ tabId: tabId, text: "off" });
+        } else {
+          browser.browserAction.setBadgeText({ tabId: tabId, text: "" });
         }
-}, { properties:['status']} );
-
+        browser.browserAction.enable(tabId);
+      } else {
+        delay_unload();
+      }
+    } else {
+      browser.browserAction.disable(tabId);
+    }
+  },
+  { properties: ["status"] }
+);
--- options.js
+++ options.js
@@ -1,64 +1,59 @@
 /* global browser */
 
 function onChange(evt) {
+  let id = evt.target.id;
+  let el = document.getElementById(id);
 
-	let id = evt.target.id;
-	let el = document.getElementById(id);
+  let value = el.type === "checkbox" ? el.checked : el.value;
+  let obj = {};
 
-	let value = ( (el.type === 'checkbox') ? el.checked : el.value)
-	let obj = {}
+  //console.log(id,value, el.type,el.min);
+  if (value === "") {
+    return;
+  }
+  if (el.type === "number") {
+    try {
+      value = parseInt(value);
+      if (isNaN(value)) {
+        value = el.min;
+      }
+      if (value < el.min) {
+        value = el.min;
+      }
+    } catch (e) {
+      value = el.min;
+    }
+  }
 
-	//console.log(id,value, el.type,el.min);
-	if(value === ""){
-		return;
-	}
-	if(el.type === 'number'){
-		try {
-			value = parseInt(value);
-			if(isNaN(value)){
-				value = el.min;
-			}
-			if(value < el.min) {
-				value = el.min;
-			}
-		}catch(e){
-			value = el.min
-		}
-	}
+  obj[id] = value;
 
-	obj[id] = value;
-
-	//console.log(id,value);
-	browser.storage.local.set(obj).catch(console.error);
-
+  //console.log(id,value);
+  browser.storage.local.set(obj).catch(console.error);
 }
 
-[ "maxactivtabs" ].map( (id) => {
+["maxactivtabs"].map((id) => {
+  browser.storage.local
+    .get(id)
+    .then((obj) => {
+      let el = document.getElementById(id);
+      let val = obj[id];
 
-	browser.storage.local.get(id).then( (obj) => {
+      if (typeof val !== "undefined") {
+        if (el.type === "checkbox") {
+          el.checked = val;
+        } else {
+          el.value = val;
+        }
+      }
+    })
+    .catch(console.error);
 
-		let el = document.getElementById(id);
-		let val = obj[id];
-
-		if(typeof val !== 'undefined') {
-			if(el.type === 'checkbox') {
-				el.checked = val;
-			}
-			else{
-				el.value = val;
-			}
-		}
-
-	}).catch( console.error );
-
-	let el = document.getElementById(id);
-	el.addEventListener('click', onChange);
-	el.addEventListener('keyup', onChange);
-	el.addEventListener('keypress',
-		function allowOnlyNumbers(event) {
-			if (event.key.length === 1 && /\D/.test(event.key)) {
-				event.preventDefault();
-			}
-		});
+  let el = document.getElementById(id);
+  el.addEventListener("click", onChange);
+  el.addEventListener("keyup", onChange);
+  el.addEventListener("keypress", function allowOnlyNumbers(event) {
+    if (event.key.length === 1 && /\D/.test(event.key)) {
+      event.preventDefault();
+    }
+  });
 });
-
